ed10d276b9e7b95a4470d0362db4e80272f9fdd4
Sean Phillips <seanphillips@protonmail.com>
2019-12-01 00:18:03 +0100

    configuring app for heroku deployment

--- src/app.js
+++ src/app.js
@@ -1,90 +1,91 @@
-const path = require("path");
-const express = require("express");
-const hbs = require("hbs");
-const forecast = require("./utils/forecast");
-const geocode = require("./utils/geocode");
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const forecast = require('./utils/forecast')
+const geocode = require('./utils/geocode')
 
-const app = express();
+const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express
-const viewsPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials");
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Set up Handlebars engine
-app.set("view engine", "hbs");
-app.set("views", viewsPath);
-hbs.registerPartials(partialsPath);
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, '../public')))
 
 // Define routes
-app.get("", (req, res) => {
-  res.render("index", {
-    title: "Weather app",
-    name: "Sean"
-  });
-});
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather app',
+    name: 'Sean'
+  })
+})
 
-app.get("/about", (req, res) => {
-  res.render("about", {
-    title: "About me"
-  });
-});
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me'
+  })
+})
 
-app.get("/weather", (req, res) => {
+app.get('/weather', (req, res) => {
   if (!req.query.address) {
     return res.send({
-      error: "You must provide an address."
-    });
+      error: 'You must provide an address.'
+    })
   }
 
   geocode(
     req.query.address,
     (error, { latitude, longitude, location } = {}) => {
       if (error) {
-        return res.send({ error });
+        return res.send({ error })
       }
 
       forecast(latitude, longitude, (error, forecastData) => {
         if (error) {
-          return res.send({ error });
+          return res.send({ error })
         }
 
         res.send({
           forecast: forecastData,
           location,
           address: req.query.address
-        });
-      });
+        })
+      })
     }
-  );
-});
+  )
+})
 
-app.get("/products", (req, res) => {
+app.get('/products', (req, res) => {
   if (!req.query.search) {
     return res.send({
-      error: "You must provide a search term."
-    });
+      error: 'You must provide a search term.'
+    })
   }
 
   res.send({
     products: []
-  });
-});
+  })
+})
 
-app.get("/help/*", (req, res) => {
-  res.render("404", {
-    message: "Help article not found."
-  });
-});
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    message: 'Help article not found.'
+  })
+})
 
-app.get("*", (req, res) => {
-  res.render("404", {
+app.get('*', (req, res) => {
+  res.render('404', {
     title: 404,
-    message: "404 page not found."
-  });
-});
+    message: '404 page not found.'
+  })
+})
 
-app.listen(3000, () => {
-  console.log("server is up on port 3000.");
-});
+app.listen(port, () => {
+  console.log('server is up on port ' + port)
+})
